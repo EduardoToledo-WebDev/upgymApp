7b3db58a3535756cf0ae82a4550fbe873c10ea4a
Abransa paulina <abransapaulina@gmail.com>
2026-03-20 09:21:19 -0700

    se agrego settings view con boton de logout

--- frontend/src/views/Inicio.jsx
+++ frontend/src/views/Inicio.jsx
@@ -1,16 +1,20 @@
 import { useOutletContext } from "react-router-dom";
 import { RachaCard } from "../components/ui/Inicio/rachaCard";
 import { Header } from "../components/ui/Inicio/header";
+import Configuracion from "./configuracion";
+import { useState } from "react";
 
 export default function Inicio() {
     const { userData } = useOutletContext();
+    const [mostrarConfiguracion, setMostrarConfiguracion] = useState(false);
 
     return (
         <div className="flex flex-col w-full h-full">
 
 
-            <Header userData={userData} />
+            <Header userData={userData} mostrarConfiguracion={mostrarConfiguracion} setMostrarConfiguracion={setMostrarConfiguracion} />
             <RachaCard racha={userData?.racha_act} estado={userData?.estado_racha} />
+            <Configuracion mostrar={mostrarConfiguracion} setMostrar={setMostrarConfiguracion} />
 
 
         </div>
